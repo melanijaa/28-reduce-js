20bb026e428b18b7152492ad15e1541cf35f1a4a
Melanija <melanija.sibakovska1@gmail.com>
2022-06-09 11:11:41 +0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
+import rand from './Functions/rand';
 import colorReducer from './Reducers/colorReducer';
+import numberReducer from './Reducers/numberReducer';
 
 function App() {
   const [color, dispachColor] = useReducer(colorReducer, 'yellow');
+  const [numb, dispachNumb] = useReducer(numberReducer, '0000');
 
   const goPink = () => {
     const action = {
@@ -33,14 +36,39 @@ function App() {
     dispachColor(action);
   }
 
+  const number1 = () => {
+    const action = {
+      type: 'do1'
+    }
+    dispachNumb(action);
+  }
+
+  const number2 = () => {
+    const action = {
+      type: 'do2', 
+      payload: rand(0, 100)
+    }
+    dispachNumb(action);
+  }
+
+  /* lemputės
+  useEffect(() => {
+    setInterval(()=> dispachColor({type: 'change_color'}), 2500)
+  }, [])
+  */
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{backgroundColor: color}}>Welcome to Reducer</h1>
+        <h1 style={{backgroundColor: color}}>Welcome to Reducer
+        <span> {numb} </span>
+        </h1>
         <button onClick={goPink}>Go pink</button>
         <button onClick={goYellow}>Go Yellow</button>
         <button onClick={goColor}>Change color</button>
         <button onClick={goChange}>Change</button>
+        <button onClick={number1}>Go 1</button>
+        <button onClick={number2}>Go 2</button>
       </header>
     </div>
   );
